132a7c60224ddff01f29eef90f4b3cc491091c8b
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2021-12-17 15:04:33 +0530

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1, 2, 6, 18])
+y = np.array([3, 10, 12, 20])
+plt.plot(x ,y ,color='red' , linestyle='dashed' , marker='o' , markerfacecolor='green' , markersize=10)
+
+plt.show()
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([12, 14, 16, 18,20,22,24])
+y = np.array([100, 200, 250, 400,300,450,500])
+plt.plot(x ,y ,color='blue' , marker='o' , markersize=10)
+plt.xlabel('Temperature in degree celcius')
+plt.ylabel('Sales')
+plt.show()
+
+
+# In[20]:
+
+
+import matplotlib.pyplot as plt
+with open("test.txt") as f:
+    data = f.read()
+data = data.split('\n')
+x = [row.split(' ')[0] for row in data]
+y = [row.split(' ')[1] for row in data]
+plt.plot(x, y)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Sample graph!')
+plt.show()
+
+
+
+
+# In[21]:
+
+
+import matplotlib.pyplot as plt
+x1 = [10,20,30]
+y1 = [20,40,10]
+plt.plot(x1, y1, label = "line 1")
+x2 = [10,20,30]
+y2 = [40,10,30]
+plt.plot(x2, y2, label = "line 2")
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Two or more lines on same plot with suitable legends ')
+plt.legend()
+plt.show()
+
+
+# In[8]:
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+fig.subplots_adjust(bottom=0.020, left=0.020, top = 0.900, right=0.800)
+
+plt.subplot(2, 1, 1)
+plt.xticks(()), plt.yticks(())
+
+plt.subplot(2, 3, 4)
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 5)
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 6)
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+
+
+
+# In[12]:
+
+
+
+
+
+# In[14]:
+
+
+import matplotlib.pyplot as plt
+x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+x_pos = [i for i, _ in enumerate(x)]
+
+plt.bar(x_pos, popularity, color=(0.4, 0.6, 0.8, 1.0))
+
+plt.xlabel("Languages")
+plt.ylabel("Popularity")
+plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.xticks(x_pos, x)
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+
+# In[22]:
+
+
+import matplotlib.pyplot as plt
+x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+x_pos = [i for i, _ in enumerate(x)]
+
+plt.bar(x_pos, popularity, color=['red', 'black', 'green', 'blue', 'yellow', 'cyan'])
+
+plt.xlabel("Languages")
+plt.ylabel("Popularity")
+plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.xticks(x_pos, x)
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+
+# In[16]:
+
+
+
+import matplotlib.pyplot as plt
+x = ['Java', 'Python', 'PHP', 'JS', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+x_pos = [i for i, _ in enumerate(x)]
+plt.barh(x_pos, popularity, color='green')
+plt.xlabel("Popularity")
+plt.ylabel("Languages")
+plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.yticks(x_pos, x)
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+
+# In[23]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_groups = 5
+men_means = (22, 30, 33, 30, 26)
+women_means = (25, 32, 30, 35, 29)
+
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+
+rects1 = plt.bar(index, men_means, bar_width,
+alpha=opacity,
+color='g',
+label='Men')
+
+rects2 = plt.bar(index + bar_width, women_means, bar_width,
+alpha=opacity,
+color='r',
+label='Women')
+
+plt.xlabel('Person')
+plt.ylabel('Scores')
+plt.title('Scores by person')
+plt.xticks(index + bar_width, ('G1', 'G2', 'G3', 'G4', 'G5'))
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+# In[24]:
+
+
+import matplotlib.pyplot as plt
+languages = 'Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++'
+popuratity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
+explode = (0.1, 0, 0, 0,0,0)  
+plt.pie(popuratity, explode=explode, labels=languages, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+plt.show()
+
+
+
+# In[19]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+df =  pd.read_csv('medal.csv')
+country_data = df["country"]
+medal_data = df["gold_medal"]
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#8c564b"]
+explode = (0.1, 0, 0, 0, 0)  
+plt.pie(medal_data, labels=country_data, explode=explode, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+plt.title("Gold medal achievements of five most successful\n"+"countries in 2016 Summer Olympics")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
