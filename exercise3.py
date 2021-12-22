b23ccd323052795b6070b2d9268b61a8486b35ab
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2021-12-22 15:04:25 +0530

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -104,6 +104,24 @@ plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
 
+#ques 6(2)
+
+
+
+import matplotlib.pyplot as plt
+x = ['Java', 'Python', 'PHP', 'JS', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+x_pos = [i for i, _ in enumerate(x)]
+plt.barh(x_pos, popularity, color='green')
+plt.xlabel("Popularity")
+plt.ylabel("Languages")
+plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.yticks(x_pos, x)
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
 
 #ques 6(3)
 
@@ -125,26 +143,9 @@ plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
 
 
-# In[16]:
 
 
-
-import matplotlib.pyplot as plt
-x = ['Java', 'Python', 'PHP', 'JS', 'C#', 'C++']
-popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
-x_pos = [i for i, _ in enumerate(x)]
-plt.barh(x_pos, popularity, color='green')
-plt.xlabel("Popularity")
-plt.ylabel("Languages")
-plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
-plt.yticks(x_pos, x)
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
-plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
-plt.show()
-
-
-# In[23]:
+# ques 7
 
 
 import numpy as np
