9cc3f330072b8ef3566319e2189efe31b2b2f5ea
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2022-01-06 15:27:26 +0530

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -185,13 +185,10 @@ plt.show()
 
 import matplotlib.pyplot as plt
 languages = 'Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++'
-popuratity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
-colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
-explode = (0.1, 0, 0, 0,0,0)  
-plt.pie(popuratity, explode=explode, labels=languages, colors=colors,
-autopct='%1.1f%%', shadow=True, startangle=140)
+popuratity = [22.2, 17.6, 8.8, 8, 77, 6.7]
+#colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"] 
+plt.pie(popuratity, labels=languages)
 
-plt.axis('equal')
 plt.show()
 
 
